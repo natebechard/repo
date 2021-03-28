39ccc9ce846c509e4215b3f1944d5cb9730ca8d1
Laughterz <80653100+Laughterz@users.noreply.github.com>
2021-03-28 13:42:45 -0400

    Add files via upload

---
+++ Assets/Movement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float vroom = 12f;
+    public float gravity = -9.81f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    bool groundTouch;
+
+    // Update is called once per frame
+    void Update()
+    {
+        groundTouch = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(groundTouch && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+        float posX = Input.GetAxis("Horizontal");
+        float posZ = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * posX + transform.forward * posZ;
+
+        controller.Move(move * vroom * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
